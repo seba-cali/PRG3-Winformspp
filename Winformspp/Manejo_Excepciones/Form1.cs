38c9b814e9ec43879e11aef9f756760b6c5f6fd0
seba-cali <sebastian.calicanton@alumnos.frgp.utn.edu.ar>
2023-04-27 04:26:03 -0300

    finally/throw added

--- Winformspp/Manejo_Excepciones/Form1.cs
+++ Winformspp/Manejo_Excepciones/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,26 +20,50 @@ namespace Manejo_Excepciones
 
 				private void btnCalcular_Click(object sender, EventArgs e)
 				{
+						int resultado;
+						try
+						{
+								resultado = calcular();
+								lblResultado.Text = "= " + resultado;
+								//Si necesito que se EJECUTE SI o SI (operacion sensible)... @finally va a ejecutar siempre
+
+						}
+						catch (Exception ex)
+						{
+								MessageBox.Show("Error: " + ex.Message);//error generico
+						}
+						finally//las instrucciones se ejecutan siempre, independientemente de si se produjo o no una excepcion
+						{
+								//---!OPERACION SENSIBLE! tiene que ejecutarse si o si.
+						}
+
+						
+						///Familias de catch//
+						//catch (OverflowException ex)
+						//catch (FormatException ex)//aca decidimos que hacer
+						//{
+						//		MessageBox.Show("Por favor, cargar solo numeros.");
+						//		//throw; nos permite lanzar excepciones, cuando no estamos manjeando la excepcion. 
+						//}
+						//catch (DivideByZeroException ex)
+						//{
+						//		MessageBox.Show("No se puede dividir por cero.");
+						//}
+				}
+				private int calcular()
+				{
 						int a, b, r;
 						try
 						{
 								a = int.Parse(txtNumero1.Text);
 								b = int.Parse(txtNumero2.Text);
 								r = a / b;
-								lblResultado.Text = "= " + r;
-						}
-						catch (FormatException ex)//aca decidimos que hacer
-						{
-								MessageBox.Show("Por favor, cargar solo numeros.");
-								//throw;
-						}
-						catch (DivideByZeroException ex)
-						{
-								MessageBox.Show("No se puede dividir por cero.");
+								return r;
 						}
 						catch (Exception ex)
 						{
-								MessageBox.Show("Error: " + ex.Message);//error generico
+								//podria guardar un registro de error en archivo.
+								throw ex;
 						}
 				}
 		}
